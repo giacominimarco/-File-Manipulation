9c6fe7e7d62e987cade96c97acb0ad3ddc2ebdbc
giacominimarco <giacomini_m.a@hotmail.com>
2020-06-18 12:11:29 -0300

    subindo repositorio

---
+++ src/components/UploadHome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoHeader from './TodoHeader'
+
+function UploadHome(){
+    return (
+        <React.Fragment>
+            <TodoHeader title="Upload files" />
+        </React.Fragment>
+    )
+}
+
+export default UploadHome;
