f8f4741d3d1ef2f7cad6b8d4e51e9d59a0524eef
giacominimarco <giacomini_m.a@hotmail.com>
2020-06-18 16:02:44 -0300

    crianto parte de upload

--- backend/uploadRouter.js
+++ backend/uploadRouter.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const multer = require('multer');
+
+const upload = multer({
+    dest: 'upload_files/',
+    fileFilter: (req, file, cb) => {
+        if(file.mimetype != 'text/plain' && !file.mimetype.includes('log')){
+            return cb(new Error('Formato de arquivo inválido.'));
+        }
+        cb(null, true);
+    }
+}).single('attachmente');
 
 router.post('/', (req,res) => {
-
+    upload(req, res, (err) => {
+        if(err) {
+            alert(err);
+            res.send(422).send();
+        }
+    })
 });
 
 module.exports = router;
