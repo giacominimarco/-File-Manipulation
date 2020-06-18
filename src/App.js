9be19567f675025d1c6ab6f225b49a1bf6dc7267
giacominimarco <giacomini_m.a@hotmail.com>
2020-06-18 12:42:53 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+import UploadHome from './components/UploadHome';
 
 import './App.css';
 
 function App() {
+    return(
+        <todoLayout>
+            <Router>
+                <Switch>
+                    <Route path="/upload">
+                        <UploadHome />
+                    </Route>
+                </Switch>
+            </Router>
+        </todoLayout>
+    )
 }
 
 export default App;
