d63380eb44945985d93d7cf6fd03bd3338b8e173
giacominimarco <giacomini_m.a@hotmail.com>
2020-06-19 12:04:27 -0300

    criate uplaod file

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const fs = require('fs');
+
+require('dotenv').config();
 
 const uploadRouter = require('./uploadRouter');
 
@@ -9,5 +12,22 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 app.use('/upload',uploadRouter);
+
+// verifica se o arquivo ou diretório já existem
+fs.access(process.env.OUTPUT, fs.constants.F_OK, (err) => {
+    if(err) {
+        fs.mkdir(process.env.OUTPUT, (err) => {
+            if(err) throw err;
+            console.log(`${process.env.OUTPUT} criado com sucesso! `)
+        })
+    }else {
+        fs.stat(process.env.OUTPUT, (err, stats) => {
+            if(err) throw err;
+            if(!stats.isDirectory()) {
+                throw new Error(`${process.env.OUTPUT} existe e não é um diretório`)
+            }
+        });
+    }
+})
 
 app.listen(port, () => console.log('Servidor Ligado'))
--- backend/uploadRouter.js
+++ backend/uploadRouter.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const multer = require('multer');
+const fs = require('fs');
+const readline = require('readline');
 
+// cria e armazena os arquivo enviados para 'upload_files/'
+// verifica se os arquivos em anexo estão no formato text/pllain e log;
 const upload = multer({
     dest: 'upload_files/',
     fileFilter: (req, file, cb) => {
@@ -9,15 +13,46 @@ const upload = multer({
         }
         cb(null, true);
     }
-}).single('attachmente');
+}).single('attachment');
 
+// tratamento do erro e envio do arquivo
 router.post('/', (req,res) => {
     upload(req, res, (err) => {
         if(err) {
-            alert(err);
+            console.log(err);
             res.send(422).send();
+        }else {
+            let file = req.file;
+            processFile(file);
+            res.send();
         }
-    })
+    });
 });
+
+
+function processFile(File) {
+    const outpath = `${process.env.OUTDIR}/${file.filename}`;
+    const writeStream = fs.createWriteStream(outpath, {
+        flags: 'a'
+    });
+    
+    // retorna caso tenha erro de escrita, não foi posivel desofuscar
+    writeStream.on('error', (err) => {
+        console.log(err);
+        throw err;
+    })
+
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream(file.path)
+    });
+
+    // imprime linha a linha o arquivo desofuscado
+    readInterface.on('line', (line) => {
+        writeStream.write(`${line.toUpperCase()}\n`)
+    })
+
+    // 1:34:00
+
+}
 
 module.exports = router;
